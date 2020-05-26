c7e11a86283ba2a2c346f51cedda62fe05b8208d
crypt0-1 <prajwal>
2020-05-26 08:54:10 +0530

    Updates

---
+++ precache-manifest.311ce8bcbcd65deba9fd3577cad43965.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfb07b30febbf5f2b33e2ffd53cf25b",
+    "url": "/restaurantFinder/index.html"
+  },
+  {
+    "revision": "0bdf0830f1d270c16c19",
+    "url": "/restaurantFinder/static/css/main.413d3a2c.chunk.css"
+  },
+  {
+    "revision": "a8b8241ece0cc00aac78",
+    "url": "/restaurantFinder/static/js/2.f81bcc88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurantFinder/static/js/2.f81bcc88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bdf0830f1d270c16c19",
+    "url": "/restaurantFinder/static/js/main.fae91260.chunk.js"
+  },
+  {
+    "revision": "83c16f464437eda15d30",
+    "url": "/restaurantFinder/static/js/runtime-main.9247636b.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/restaurantFinder/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/restaurantFinder/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
